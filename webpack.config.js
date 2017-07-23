518cb6b50fde2cbbd096c2db3c32e94bc284c86c
Hebert Porto <hebertporto@gmail.com>
2017-07-23 17:33:41 -0300

    ajustando conflito de versao no webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,41 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   entry: './src/index.jsx',
   output: {
-    path: __dirname + '/public',
-    filename: './bundle.js'
+    path: `${__dirname}/public`,
+    filename: './bundle.js',
   },
   devServer: {
     port: 8080,
-    contentBase: './public'
+    contentBase: './public',
   },
   plugins: [
-    new ExtractTextPlugin("app.css"),
+    new ExtractTextPlugin('app.css'),
   ],
   module: {
-    preLoaders: [{
-      test: /.js[x]?$/,
-      exclude: [/node_modules/, /libs/],
-      loader: 'eslint-loader'
-    }],
+    rules: [
+      {
+        test: /.jsx?$/,
+        enforce: 'pre',
+        exclude: [/node_modules/, /libs/],
+        loader: 'eslint-loader',
+      },
+    ],
     loaders: [{
-      test: /.js[x]?$/,
+      test: /.jsx?$/,
       loader: 'babel-loader',
       exclude: /node_modules/,
-      query:{
-        presets: ['es2015', 'react', 'stage-3']
-      }
-    },{
+      query: {
+        presets: ['es2015', 'react', 'stage-3'],
+      },
+    }, {
       test: /\.css$/,
       loader: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: "css-loader"
-        })
-    }]
-  }
+        fallback: 'style-loader',
+        use: 'css-loader',
+      }),
+    }],
+  },
 }
